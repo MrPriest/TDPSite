2b7028adf29b99c3e190f7bbe7b01ca8dd3f0b5d
Thomas Del Prete <thomas.d.p@hotmail.it>
2021-06-27 01:52:13 +0200

    added a small temporary fix for logo behaviour

--- js/custom.js
+++ js/custom.js
@@ -84,3 +84,17 @@ $(document).ready(function() {
 
   });
 
+  /* Extra
+  -----------------------------------------------*/
+  // When the user scrolls down 50px from the top of the document, resize the header's font size
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById("logo").style.width = "30px";
+      document.getElementById("logo").style.height = "30px";
+    } else {
+      document.getElementById("logo").style.width = "70px";
+      document.getElementById("logo").style.height = "70px";
+    }
+  }
